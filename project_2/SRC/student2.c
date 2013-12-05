f26541335f0950dffda3a7e99201d8b06bed382c
Ted Meyer <tmathmeyer@gmail.com>
2013-12-04 22:18:43 -0500

    yesss

--- project_2/SRC/student2.c
+++ project_2/SRC/student2.c
@@ -29,7 +29,7 @@
  */
 
 
-int teds_awesome_checksum(char* data, int data_len)
+int teds_awesome_checksum(char *data, int data_len, int acknum, int seqnum)
 {
 	long chk = data_len ^ data[0];
 	int i = 1;
@@ -40,16 +40,79 @@ int teds_awesome_checksum(char* data, int data_len)
 	}
 	int a = chk;
 	int b = chk >> 32;
-	return a^b;
+	return a^b^acknum^seqnum;
+}
+
+int AREC_packet_number = 0;
+
+struct ll {
+	struct pkt* packet;
+	struct ll* next;
+};
+
+struct ll* head = 0;
+
+void enqueue_packet(struct pkt* packet)
+{
+	struct ll* new_head = malloc(sizeof(struct ll));
+	new_head->packet = packet;
+	new_head->next = head;
+	head = new_head;
+}
+
+void clear_packets_before(int last_needed_seq_num)
+{
+	struct ll* top = head;
+	while(top != 0)
+	{
+		if (top->packet->seqnum < last_needed_seq_num)
+		{
+			top->next = 0;
+		}
+		top = top->next;
+	}
+}
+
+void write_all_packets_in_buffer()
+{
+	struct ll* top = head;
+	while(top != 0)
+	{
+		tolayer3(0, *(top->packet));
+		top = top->next;
+	}
+}
+
+void write_eight_packets_in_buffer(int i)
+{
+	struct ll* top = head;
+	while(top != 0)
+	{
+		if (top->packet->seqnum - i <= 7)
+		{
+			tolayer3(0, *(top->packet));
+		}
+		top = top->next;
+	}
 }
 
 
-int AREC_packet_number = 0;
-int AREC_ack_number = 0;
-int AREC_last_send_time = 0;
 
-int BREC_packet_number = 0;
-int BREC_acknack_number = 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -63,16 +126,23 @@ int BREC_acknack_number = 0;
  * in-order, and correctly, to the receiving side upper layer.
  */
 void A_output(struct msg message) {
-	AREC_packet_number++;
 	struct pkt packet;
 	packet.seqnum = AREC_packet_number;
 	packet.acknum = -1;
-	packet.checksum = teds_awesome_checksum(message.data, MESSAGE_LENGTH);
+	packet.checksum = teds_awesome_checksum(message.data, MESSAGE_LENGTH, -1, packet.seqnum);
 	strncpy(packet.payload, message.data, MESSAGE_LENGTH);
-
-
+	
+	AREC_packet_number += 1;
 	tolayer3(0, packet);
+
+	struct pkt* packetQ = malloc(sizeof(struct pkt));
+	memcpy(packetQ, &packet, sizeof(struct pkt));
+
+	enqueue_packet(packetQ);
 }
+
+
+
 
 /*
  * Just like A_output, but residing on the B side.  USED only when the 
@@ -89,13 +159,26 @@ void B_output(struct msg message)  {
  * packet is the (possibly corrupted) packet sent from the B-side.
  */
 void A_input(struct pkt packet) {
-	if (teds_awesome_checksum(packet.payload, MESSAGE_LENGTH) == packet.checksum && packet.acknum == 1)
-	{
-		AREC_ack_number++;
+//	if (teds_awesome_checksum(packet.payload, MESSAGE_LENGTH) == packet.checksum && packet.acknum == 1)
+//	{
+//		AREC_ack_number++;
+//	}
+//	else
+//	{
+//	}
+
+	int next_packet = packet.acknum;
+	int npc = packet.checksum;
+
+	
+	if (npc == teds_awesome_checksum(packet.payload, MESSAGE_LENGTH, packet.acknum, packet.seqnum))
+	{	
+		printf("ACK ON:  %i\n-->last:  %i\n", next_packet, head->packet->seqnum);
+		clear_packets_before(next_packet);
+		write_eight_packets_in_buffer(next_packet);
 	}
-	else
-	{
-	}
+
+//	printf("ACK ON:  %i\nCKSM:  %i\n",next_packet, npc);
 }
 
 /*
@@ -107,6 +190,7 @@ void A_input(struct pkt packet) {
 void A_timerinterrupt() {
 	puts("A_ti");
 }  
+
 
 /* The following routine will be called once (only) before any other    */
 /* entity A routines are called. You can use it to do any initialization */
@@ -124,21 +208,39 @@ void A_init() {
  * of a tolayer3() being done by a A-side procedure) arrives at the B-side. 
  * packet is the (possibly corrupted) packet sent from the A-side.
  */
+int next_req_pkt = 0;
+
 void B_input(struct pkt packet) {
-
-	if (teds_awesome_checksum(packet.payload, MESSAGE_LENGTH) == packet.checksum)
+	char* got_pay = packet.payload;
+	int len = MESSAGE_LENGTH;
+	int got_ack = packet.acknum;
+	int got_seq = packet.seqnum;
+	if (teds_awesome_checksum(got_pay, len, got_ack, got_seq) == packet.checksum)
 	{ //the packet is valid!!
-		struct msg m;
-		strncpy(m.data, packet.payload, MESSAGE_LENGTH);
-		struct pkt return_packet;
-
-		return_packet.acknum=1;
-		strncpy(return_packet.payload, packet.payload, MESSAGE_LENGTH);
-		return_packet.checksum = packet.checksum;
-		return_packet.seqnum = packet.seqnum;
-
-		tolayer3(1, return_packet);
-		tolayer5(1, m);
+		if (packet.seqnum > next_req_pkt)
+		{
+			struct pkt ack_dropped;
+			strncpy(packet.payload, "                               ", MESSAGE_LENGTH);
+			ack_dropped.acknum = next_req_pkt;
+			ack_dropped.seqnum = next_req_pkt;
+			ack_dropped.checksum = teds_awesome_checksum(ack_dropped.payload, MESSAGE_LENGTH, ack_dropped.acknum, ack_dropped.seqnum); 
+			
+			
+			tolayer3(1, ack_dropped);
+			return;
+		}
+		else if (packet.seqnum < next_req_pkt)
+		{
+			return; //you've already gotten this one
+		}
+		else
+		{ //send the message back up and increase counter
+			struct msg m;
+			strncpy(m.data, packet.payload, MESSAGE_LENGTH);
+			tolayer5(1, m);
+			next_req_pkt += 1;
+			return;
+		}
 	}
 	else
 	{ //the packet is a pile of shit
@@ -146,13 +248,14 @@ void B_input(struct pkt packet) {
 		struct pkt return_packet;
 
 		strncpy(m.data, packet.payload, MESSAGE_LENGTH);
-		strncpy(return_packet.payload, packet.payload, MESSAGE_LENGTH);
-		return_packet.acknum = 0;
-		return_packet.checksum = packet.checksum;
-		return_packet.seqnum = packet.seqnum;
+		strncpy(return_packet.payload, "                                      ", MESSAGE_LENGTH);
+
+		return_packet.acknum = next_req_pkt;
+		return_packet.seqnum = next_req_pkt;
+		return_packet.checksum = teds_awesome_checksum(return_packet.payload, MESSAGE_LENGTH, return_packet.acknum, return_packet.seqnum);
 		
 		tolayer3(1, packet);
-		tolayer5(1, m);
+		//tolayer5(1, m);
 	}
 }
 
